dc269413728ed1873975c17974ce5aa2bd0ef698
Dima <yadima2001@gmail.com>
2022-01-04 13:37:22 +0200

    message box

--- Module3HW6/Program.cs
+++ Module3HW6/Program.cs
@@ -7,6 +7,25 @@ namespace Module3HW6
     {
         public static void Main(string[] args)
         {
+            var messageBox = new MessageBox();
+            var tcs = new TaskCompletionSource<State>();
+            Action<State> action = (x) =>
+            {
+                if (x == State.Cancel)
+                {
+                    Console.WriteLine("Operation rejected");
+                }
+                else
+                {
+                    Console.WriteLine("Operation accept");
+                }
+
+                tcs.SetResult(x);
+            };
+            messageBox.WindowClose += action;
+            messageBox.Open();
+
+            tcs.Task.GetAwaiter().GetResult();
         }
     }
 }
