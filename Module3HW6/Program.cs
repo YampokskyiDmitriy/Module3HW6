a4186f752684a43fb155be5ca07ff496a567d632
Dima <yadima2001@gmail.com>
2022-01-04 13:35:28 +0200

    Add project files.

---
+++ Module3HW6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Module3HW6
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
